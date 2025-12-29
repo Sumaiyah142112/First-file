2f6aeaaa8b7dcebe73b7695ef4087bb7eaaa363d
Sumaiyah142112 <Airajunnisa@gmail.com>
2025-12-29 19:18:24 +0300

    Add files via upload

---
+++ String Operations.py
@@ -0,0 +1,15 @@
+first_name ="Codingal"
+last_name ="Education"
+full_name =last_name+first_name
+example = "Haa"*5
+
+print("First Name: ",first_name)
+print("Last Name: ",last_name)
+print("Full Name: ",full_name)
+print("String multiplied 5 times gives the result: ",example)
+
+word="Coding"
+print("Length of String:", len(word))
+print("First letter of String:", word[0])
+print("Last letter of String:", word[5])
+print("String sliced:",word[1:3])
