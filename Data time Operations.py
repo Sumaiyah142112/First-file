d43864ed9ad933ea086fffe43b1c4c0561e71d06
Sumaiyah142112 <Airajunnisa@gmail.com>
2026-01-02 16:17:55 +0300

    Add files via upload

---
+++ Data time Operations.py
@@ -0,0 +1,11 @@
+import datetime
+# using now() to get current time
+current_time = datetime.datetime.now()
+ # Printing value of now
+print("Time now at greenwich meridian : ", end = "")
+print(current_time)
+
+#Print calendar of year 2026
+import calendar
+print("\n",calendar.calendar(2026))
+
