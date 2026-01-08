48651572ee5a090152a7537749b0de834e501edd
Sumaiyah142112 <Airajunnisa@gmail.com>
2026-01-08 17:18:03 +0300

    Add files via upload

---
+++ NAtural numbers Sum.py
@@ -0,0 +1,4 @@
+#Python program to print the 1st ten natural numbers
+# Use built in sum and range functions to print the sum
+total_sum= sum(range(1,11))
+print(f"The sum of the first ten natural numbers is {total_sum}")
