3ca2d146153bd5ac390322d1867643f69d2d1b4a
Sumaiyah142112 <Airajunnisa@gmail.com>
2026-02-03 17:04:43 +0300

    Add files via upload

---
+++ My student - 2.py
@@ -0,0 +1,16 @@
+class student:
+    grade = 10
+    name = "penguin"
+    
+    def introduction(self):
+      print("Hi I am a student")
+    
+    def detalis(self):
+      print("My name is ",self.name)
+      print("I study in grade",self.grade)
+
+ob = student()
+ob.introduction()
+ob.detalis()    
+
+    
