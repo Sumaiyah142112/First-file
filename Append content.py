acb4061b21c2d4d2783cf55a91abbe8068c22b6e
Sumaiyah142112 <Airajunnisa@gmail.com>
2026-02-19 17:29:55 +0300

    Add files via upload

---
+++ Append content.py
@@ -0,0 +1,26 @@
+firstfile = input("Enter the name of ur first file ")
+secondfile = input("Enter the name of ur second file ")
+
+f1= open(firstfile , 'a+')
+f2= open(secondfile , 'r')
+
+print ("Content of first file before appending - \n",f1.read())
+print ("Content of second file before appending - \n",f2.read())
+
+f1.close()
+f2.close()
+
+f1 = open('firstfile' , 'a')
+f2 = open('secondfile' , 'r')
+
+f1.write(f2.read())
+
+f1.seek(0)
+f2.seek(0)
+
+print ("Content of first file after appending - \n",f1.read())
+print ("Content of second file after appending - \n",f2.read())
+
+f1.close()
+f2.close()
+
