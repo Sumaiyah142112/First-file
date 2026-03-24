99178c0cbb5a39f6d08f11c8a94ab56e61a07619
Sumaiyah142112 <Airajunnisa@gmail.com>
2026-03-24 17:21:34 +0300

    Add files via upload

---
+++ LoopTIme.py
@@ -0,0 +1,16 @@
+def myfunction(n):
+    for i in range(0,n+1):
+        print("First Loop")
+ 
+    j=1
+    while(j<=n+1):
+        print("Second Loop ",j)
+        j=j*2
+ 
+    for i in range(0,100):
+        print("Third loop")
+
+print("First Loop: O(n)")
+print("Second Loop: O(log n)")
+print("First Loop: O(1)")
+print("Time Complexity: O(n)")
