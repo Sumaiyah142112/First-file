2a68a2bf7295db7a11487e3b079e43ba5353567c
Sumaiyah142112 <Airajunnisa@gmail.com>
2026-03-24 16:28:13 +0300

    Add files via upload

---
+++ O(n^2).py
@@ -0,0 +1,14 @@
+def OnSquareTime(n):
+    iteration=0
+    for i in range(0,n):
+        for j in range(0,n):
+            print("*",end=" ")
+            iteration+=1
+            print(" ")
+            print("\nWhen n is ",n,"Itearations = ",iteration,"\n")
+OnSquareTime(5)    
+OnSquareTime(4)  
+OnSquareTime(3)  
+
+print("\nWith every 'n' the time taken equals n^2")
+print("O(n^2)")
