980309898fee83c0ac5d85ff32143e38fe967ae8
Sumaiyah142112 <Airajunnisa@gmail.com>
2026-03-29 20:24:32 +0300

    Add files via upload

---
+++ Binary to Decimals.py
@@ -0,0 +1,15 @@
+#Convert Binary to Decimals
+print("Binary to Decimals")
+number = int(input("Type a binary number: "),2)
+
+print("Decimal: ",number)
+
+#Convert Decimals to Binary
+print("Decimals to Binary")
+decimal = int(input("Enter a number: "))
+decimal = bin(decimal)
+decimal = decimal[2:]
+
+print("Binary: ",decimal)
+
+    
